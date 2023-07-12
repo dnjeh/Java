125d47e8813e15412707855a3455308cf4c24c52
dnjeh <ehdnj.001@gmail.com>
2023-07-12 09:44:03 +0900

    Today's abstractClass's source codes'

---
+++ SRHclass06/_20230712/src/kr/ac/kw/_20230712/Shape.java
@@ -0,0 +1,7 @@
+package kr.ac.kw._20230712;
+
+public abstract class Shape {
+	public abstract double getCircum();
+	public abstract double getArea();
+}
+
---
+++ SRHclass06/_20230712/src/kr/ac/kw/_20230712/Ship.java
@@ -0,0 +1,6 @@
+package kr.ac.kw._20230712;
+
+public abstract class Ship {
+	public abstract int Passenger();
+	public abstract int Luggage();
+}
